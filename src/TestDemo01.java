08a23238e6e0223c6f242a774a94bd29e9a5804f
jhappye <jiangha6237@163.com>
2021-11-08 15:43:09 +0800

    增加 “；”号

--- src/TestDemo01.java
+++ src/TestDemo01.java
@@ -10,7 +10,7 @@ public class TestDemo01 {
                 while(true){
                     testList.add(new TestDemo01());
                     Thread.sleep(10);
-                    System.out.println("dev")
+                    System.out.println("dev");
                 }
     }
 }
