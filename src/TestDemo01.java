9e16d9f44a40611bb239b3a41149ef5260b0a3c5
jhappye <jiangha6237@163.com>
2021-11-08 14:57:14 +0800

    init

---
+++ src/TestDemo01.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class TestDemo01 {
+
+    byte[] a = new byte[1024 * 100];
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ArrayList<TestDemo01> testList = new ArrayList<>();
+                while(true){
+                    testList.add(new TestDemo01());
+                    Thread.sleep(10);
+                }
+    }
+}
